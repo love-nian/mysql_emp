296f001b3cc72da92a3662b8be2c7ffe43003967
love-nian <745729377@qq.com>
2016-09-28 08:31:01 +0800

    1

---
+++ mysql_emp/src/com/mo/controller/EmpController.java
@@ -0,0 +1,58 @@
+package com.mo.controller;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mo.entity.Emp;
+import com.mo.entity.Pager;
+import com.mo.service.EmpService;
+
+/**
+ * 员工控制器
+ * 当不继承相关类或是实现相关接口的时候，
+ * 就只能通过注解的方式来编写，达到 注入业务逻辑层的目的
+ * 如果要把多个员工的业务方法写到一个控制器中统一管理，就只能通过这种方式
+ * @author Lenovo
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class EmpController {
+	private static final Logger LOGGER = Logger.getLogger(EmpController.class);
+	@Autowired
+	private EmpService empService;
+	public void setEmpService(EmpService empService) {
+		this.empService = empService;
+	}
+
+	/**
+	 * 查询所有员工信息
+	 * @param pager 分页实体类
+	 * @param emp 条件对象（负责接收条件）
+	 * @param model 模型对象（负责把数据传递到页面）
+	 * @return
+	 */
+	@RequestMapping(value="findEmps.do",method={RequestMethod.GET})
+	public String findEmps(Pager pager,Emp emp,Model model){
+		try {
+			//调用业务层的方法得到员工总记录数并将其封装到pager对象中
+			pager.setCounts(empService.getCount());
+			//调用业务逻辑层的方法查询所有的员工
+			List<Emp> emps = empService.findEmps(pager, emp);
+			//把查询得到的员工信息封装到model对象中，传递到页面
+			model.addAttribute("emp", emps);
+			model.addAttribute("pager", pager);
+			return "emp_list";
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			return null;
+		}
+	}
+
+}
